e25afe96dd37913d0ad119b258f450d3010e863a
陳建佑 <jianyou224@gmail.com>
2024-09-17 13:31:06 +0800

    0917

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>PDF Export</h1>
+    <div class="export_container" id="export_container">
+      <div class="value_container">
+        <h3>日期： {{ getYMD() }}</h3>
+        <h3>耗電量： {{ '272 kWh' }}</h3>
+      </div>
+      <div class="chart_container">
+        <SampleChart class="my-chart"/>
+      </div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic dolore sit laboriosam consequuntur accusantium? Maxime facere eos alias officiis rem ipsa similique inventore facilis asperiores ab, rerum eius, modi vero? Ut, beatae nam? Incidunt, harum eligendi, numquam magnam vero optio ea voluptatibus libero ipsam a, reiciendis dolorem dignissimos quam quas? Iusto odit veritatis illum praesentium fugit dolorem eius ipsa, ducimus ullam quae aliquam officia, hic dignissimos obcaecati quo omnis laboriosam reprehenderit suscipit, neque voluptatum iste harum non amet. Reiciendis tenetur a odio officia unde ipsa atque, beatae perferendis.</p>
+    </div>
+    
+    <button class="export_btn" @click="exportPDF()">輸出</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SampleChart from './components/SampleChart.vue';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { SampleChart},
+  methods: {
+    exportPDF() {
+      const el = document.getElementById('export_container');
+      // const el = document.querySelector('.export_container');
+      html2canvas(el).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'PNG', 0, 0);
+        pdf.save('test.pdf');
+      });
+    },
+    getYMD() {
+      const date = new Date();
+      const yy = date.getFullYear();
+      const mm = date.getMonth() + 1;
+      const dd = date.getDate();
+      return `${yy}/${mm}/${dd}`
+    },
+  },
 }
 </script>
 
@@ -25,4 +55,42 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+h1{
+  margin-bottom: 3rem;
+}
+
+.export_container{
+  width: 770px;
+  /* !!!寬度會影響到PDF輸出 */
+  margin: 0 auto;
+}
+
+.value_container{
+  width: 70%;
+  margin: 0 auto ;
+  display: flex;
+  justify-content: center;
+
+  h3{
+    margin: 2rem;
+  }
+}
+
+.chart_container{
+  width: 90%;
+  margin: 0 auto 3rem;
+  /* border: 1px solid #f00; */
+  
+  .my-chart{
+    height: 500px;
+  }
+}
+
+button.export_btn{
+  font-size: 20px;
+  padding: .5rem 1.5rem;
+}
+
+
 </style>
---
+++ src/components/SampleChart.vue
@@ -0,0 +1,104 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  // props: ['CO2Array', 'PM25Array', 'XAxis'],
+  data: () => ({
+    chartdata: {
+      labels: [1,2,3,4,5],
+      datasets: [
+        {
+          label: 'CO2',
+          backgroundColor: '#fa0',
+          borderColor: '#fa0',
+          data: [10,40,50,60,57],
+          fill: false,
+          yAxisID: 'chart_co2',
+          lineTension: 0, // 設置為0以禁用曲線
+        }
+      ]
+    },
+    options: {
+      responsive: true, // 預設true
+      maintainAspectRatio: false, // 預設false
+      tooltips: { // hover 顯示的設定
+        backgroundColor: '#33333390',
+        titleFontSize: 20,
+        bodyFontSize: 20
+      },
+      title:{
+        display: false,
+        text: '標題標題',
+        fontColor: '#fff',
+        fontSize: 40,
+        position: 'bottom',
+      },
+      legend: { //標籤
+        display: false,
+        position: 'top',
+        align: 'end',
+        labels: {
+          fontColor: '#fff',
+          fontSize: 24,
+        }
+      },
+      scales : {
+        yAxes:[
+          {
+            id: 'chart_co2',
+            // type: 'linear',
+            ticks: {
+              fontColor: '#333',
+              fontSize: '18',
+              beginAtZero: true, // 從0開始
+              // fixedStepSize: 10 // 設置固定的步長
+              display: true,
+            },
+            gridLines: {
+              display: true,
+              color: '#aaaaaa80'
+            },
+            scaleLabel: {
+              display: false,
+              labelString: 'CO2',
+              fontColor: '#fa0',
+              fontSize: 20
+            },
+            position: 'left',
+          }
+        ],
+        xAxes:[
+          {
+            ticks: { // x軸文字
+              fontColor: "#333",
+              fontSize: 18,
+              // autoSkip: true,
+              // maxTicksLimit: 10 // x軸總共秀幾個item
+            },
+            gridLines: { //x軸網格線
+              display: false,
+              color: "#888"
+            },
+            scaleLabel: {
+              display: false,
+              labelString: '時間',
+              fontColor: '#fff',
+              fontSize: 20
+            },
+          }
+        ],
+      },
+    },
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  },
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
